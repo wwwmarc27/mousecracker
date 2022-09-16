9fc43df57c034eaa15d4c497588684a119a0b383
Marcel Blankschein <marcel_blankschein@web.de>
2022-09-16 13:25:56 +0200

    Kollision mit Gegner

--- kotlin/clocks/Clock.kt
+++ kotlin/clocks/Clock.kt
@@ -2,6 +2,7 @@ package clocks
 
 import gui.Draw
 import gui.Gui
+import player.RealPlayer
 
 class Clock : Thread() {
 
@@ -20,11 +21,21 @@ class Clock : Thread() {
 
             //Collision
             for (element in Draw.opponent) {
+
+                //Prüfen ob Gegner an Fensterrand anstoßen
+                //wenn ja, Richtung ändern
                 if(element.richtungRechts && element.x >= Gui.WIDTH - 25) {
                     element.richtungRechts = false
                 } else if (!element.richtungRechts && element.x <= 0) {
                     element.richtungRechts = true
                 }
+
+                if(element in Draw.opponent) {
+                    if(element.x >= RealPlayer.x - 5 && element.x <= RealPlayer.x + 30 && element.y >= RealPlayer.y + 5 && element.y <= RealPlayer.y + 35) {
+                        println("Collision")
+                    }
+                }
+
             }
 
             sleep(10)
