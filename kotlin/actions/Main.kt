65b85caa6ba81d7a3adf393af857eb98e83e036f
Marcel Blankschein <marcel_blankschein@web.de>
2022-09-16 21:52:36 +0200

    CollisionClock hinzugefügt und gestartet

--- kotlin/actions/Main.kt
+++ kotlin/actions/Main.kt
@@ -1,11 +1,13 @@
 package actions
 
-import clocks.Clock
+import clocks.CollisionClock
+import clocks.MovementClock
 import gui.Gui
 import player.RealPlayer
 
 fun main(args: Array<String>) {
     RealPlayer()
-    Clock().start()
+    MovementClock().start()
+    CollisionClock().start()
     Gui()
 }
