d422d049d8af0cda9ee51496b838a7d10e9e359f
Marcel Blankschein <marcel_blankschein@web.de>
2022-09-17 23:54:36 +0200

    FontLoader.kt hinzugefügt

--- kotlin/actions/Main.kt
+++ kotlin/actions/Main.kt
@@ -2,11 +2,13 @@ package actions
 
 import clocks.CollisionClock
 import clocks.MovementClock
+import gui.FontLoader
 import gui.Gui
 import player.RealPlayer
 
 fun main(args: Array<String>) {
     RealPlayer()
+    FontLoader()
     MovementClock().start()
     CollisionClock().start()
     Gui()
