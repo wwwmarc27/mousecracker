03d1f57638e8f3e2cc9942bf85f0a9bacc66a02d
Marcel Blankschein <marcel_blankschein@web.de>
2022-09-17 23:57:07 +0200

    Schriftart laden

--- kotlin/gui/FontLoader.kt
+++ kotlin/gui/FontLoader.kt
@@ -1,1 +1,19 @@
 package gui
+
+import java.awt.Font
+import java.io.File
+
+class FontLoader()  {
+
+    companion object {
+        lateinit var font: Font
+    }
+
+    init {
+        try {
+            font = Font.createFont(Font.TRUETYPE_FONT, File("src/main/resources/aureola.regular.ttf"))
+        } catch (e: Exception) {
+            println(e.stackTrace)
+        }
+    }
+}
