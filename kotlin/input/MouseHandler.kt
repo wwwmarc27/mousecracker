4c4788ca288ab167dcd796b9d6534ae69502ca47
Marcel Blankschein <marcel_blankschein@web.de>
2022-09-18 10:44:05 +0200

    Hitbox für New Game Button

--- kotlin/input/MouseHandler.kt
+++ kotlin/input/MouseHandler.kt
@@ -1,5 +1,6 @@
 package input
 
+import gui.Draw
 import player.RealPlayer
 import java.awt.event.MouseEvent
 import java.awt.event.MouseListener
@@ -8,13 +9,20 @@ import java.awt.event.MouseMotionListener
 class MouseHandler : MouseListener, MouseMotionListener {
 
     override fun mouseDragged(e: MouseEvent) {
+        //dragged playerImage
         if(((e.x >= RealPlayer.x - 10 && e.x <= RealPlayer.x + 40) && (e.y >= RealPlayer.y + 20 && e.y <= RealPlayer.y + 70)) && ((!RealPlayer.crashed) && (!RealPlayer.finish))) {
             RealPlayer.x = e.x - 12
             RealPlayer.y = e.y - 40
         }
     }
 
-    override fun mouseClicked(e: MouseEvent) {}
+    override fun mouseClicked(e: MouseEvent) {
+        //Hitbox button New Game
+        if((e.x >= Draw.buttonNewGame.x && e.x <= Draw.buttonNewGame.x + 150 && e.y >= Draw.buttonNewGame.y + 20 && e.y <= (Draw.buttonNewGame.y + 45)) && (RealPlayer.crashed)) {
+            //Funktioniert
+            //mit Inhalt füllen
+        }
+    }
 
     override fun mousePressed(e: MouseEvent) {}
 
