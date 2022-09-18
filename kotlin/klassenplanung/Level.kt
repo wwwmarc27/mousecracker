71d95b8efca3b8246c7c31573fb158c75026558f
Marcel Blankschein <marcel_blankschein@web.de>
2022-09-18 11:04:34 +0200

    Level-Klasse implementiert

---
+++ kotlin/klassenplanung/Level.kt
@@ -0,0 +1,13 @@
+package klassenplanung
+
+import gui.Draw
+import player.Opponents
+
+object Level {
+    var level: Int = 0
+
+    fun addOpponents(x: Int, y: Int, bewegungsSchritt: Int, richtungRechts: Boolean) {
+        Draw.opponents.add(Opponents(x, y, bewegungsSchritt, richtungRechts))
+    }
+
+}
