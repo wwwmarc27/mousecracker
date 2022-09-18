2b126d32e356f2129cf6782ac56a41f1c81aa7d6
Marcel Blankschein <marcel_blankschein@web.de>
2022-09-18 10:42:41 +0200

    Variable für Farbe hinzugefügt

--- kotlin/actions/MyButton.kt
+++ kotlin/actions/MyButton.kt
@@ -1,10 +1,13 @@
 package actions
 
-class MyButton(x: Int, y: Int, width: Int, height: Int, text: String) {
+import java.awt.Color
+
+class MyButton(x: Int, y: Int, width: Int, height: Int, text: String, color: Color) {
     val x: Int = x
     val y: Int = y
     val width: Int = width
     val height: Int = height
     val text: String = text
+    val basicColor: Color = color
     var hover: Boolean = false
 }
